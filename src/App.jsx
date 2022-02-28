5938138aeb0d83fefc403247f73a8eb0ce1b0991
qabusari <quddusbusari@outlook.com>
2022-02-28 19:43:21 +0100

    create conceptualised empty pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import Home from './pages/Home';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+
+const App = () => {
+  return <Home />;
+};
 
 export default App;
