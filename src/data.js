c00fe32d44ffabe1665f68d688669658f9b59072
qabusari <quddusbusari@outlook.com>
2022-03-01 05:59:17 +0100

    create product and products components, add products component to home page

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const sliderItems = [
   },
   {
     id: 2,
-    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    img: 'https://i.ibb.co/bRF1sbD/wara-newslide2.png',
     title: 'AUTUMN COLLECTION',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: 'fcf1ed',
@@ -47,7 +47,7 @@ export const popularProducts = [
   },
   {
     id: 2,
-    img: 'https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388',
+    img: 'https://i.ibb.co/xhpKLNY/Ripped-Black-Shorts-PNG-File-Download-Free.png',
   },
   {
     id: 3,
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: 'https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png',
+    img: 'https://i.ibb.co/dk2Fn32/Denim-Jacket-Download-PNG-Image.png',
   },
   {
     id: 8,
